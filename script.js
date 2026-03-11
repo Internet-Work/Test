1e72b8ba8dee01e69ea0dd86253b798912806685
Vinnie <vmatronia@gmail.com>
2026-03-10 21:40:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Script v0
+
+console.log("[Script] Loading...");
+
+function testFunction() {
+  console.log("[Script] testFunction");
+  alert("This is a test function.");
+}
+
+console.log("[Script] Loaded.");
